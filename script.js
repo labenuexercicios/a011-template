7f46d99cd5608edd614400d5aa9cfb57511d2882
Alesouza7 <alexcostasouza86@yahoo.com>
2022-08-01 21:49:01 -0300

    Aula Concluida

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//criação de objeto
+/*criação de objeto
 const estudante = {
     nome: "alex", 
     sobrenome:"costa",
@@ -9,9 +9,9 @@ const estudante = {
 // adicionando nova propriedade ao objeto
 estudante.modulo="HTML E CSS"
 
-/* imprimindo informações do objeto estudante
+// imprimindo informações do objeto estudante
 
-console.log("Objeto estudante",estudante.semestre[1]) */
+console.log("Objeto estudante",estudante.semestre[1])
 
 //fzendo copia do objeto estudante spread ou espalhamento
 
@@ -34,4 +34,36 @@ const estudanteLabenu =[]
 estudanteLabenu.push("Estudante",estudante)
 estudanteLabenu.push("Copia estudante",copiaEstudante)
 
-console.log(estudanteLabenu)
+console.log(estudanteLabenu)*/
+
+const carrinho = {nome:"alex",formaPag:"pix",endereco:"brasil"}
+
+carrinho.compras=[
+    {
+compras:"lapis",
+preco:5.99,
+quantidade:30
+},
+{
+    compras:"tesoura",
+    preco:19.99,
+    quantidade:39
+    },   
+    {
+        compras:"mochila",
+        preco:20.99,
+        quantidade:10
+        }
+]
+
+const carrinhoPresente = {...carrinho}
+carrinhoPresente.nome="Juliana"
+carrinhoPresente.formaPag="cartão presente"
+
+console.log("Objeto de compras",carrinho.compras[1])
+
+console.log("Carrinho original",carrinho)
+
+console.log("Copia carrinho",carrinhoPresente)
+
+
